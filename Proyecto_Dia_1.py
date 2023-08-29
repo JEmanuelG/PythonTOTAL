a7bbc5d647effe9e37fa4b19ee5aa6a74f8bc78a
Emanuel Guaraz <guarazjemanuel@gmail.com>
2023-08-28 21:11:29 -0300

    Resolution of the first project

---
+++ Proyecto_Dia_1.py
@@ -0,0 +1,6 @@
+print("Bienvenido! Nombrá tu producto")
+
+x = input("Cuál es/era el nombre de tu primer mascota?:")
+y = input("Cuál es tu color favorito?:")
+
+print('El nombre de tu producto podría ser:\n' + '"' + x + y + '"')
