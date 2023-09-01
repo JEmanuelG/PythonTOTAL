1ad9ac32f22bf33f201817609b334abe9cd91902
Emanuel Guaraz <guarazjemanuel@gmail.com>
2023-08-31 22:08:22 -0300

    Second exercice resolution

---
+++ proyecto_dia_2.py
@@ -0,0 +1,7 @@
+
+name = str(input("Cuál es tu nombre?: "))
+sales = float(input("Cuánto vendiste en total este mes?: $"))
+
+commissions = round(sales * 13 / 100, 2)
+
+print(f"Ok, {name}. Este mes ganaste ${commissions}")
